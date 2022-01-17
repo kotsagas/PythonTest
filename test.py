12f75b96f7df38635a937ed1a05f346c3b475c26
Ioannis Kotsagas <kotsagas@gmail.com>
2022-01-17 10:19:37 +0200

    Add check for integer

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ os.system("clear")
 
 x = int(input("Enter an integer: "))
 
+if x > 1000:
+    print("Number greater than 1000, terminating program")
+    exit()
+
 print("\n")
 
 def my_function(z):
